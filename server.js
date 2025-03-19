19deb916f95e48ee75c34eaf69d80452d4cc02a7
Manu0601 <manu06011998@gmail.com>
2025-03-19 14:48:18 +0530

    update 13

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 import express from "express";
 import { connectDb } from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { apiRouter } from "./routes/version_1/index.js";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api",apiRouter)
+app.use("/api", apiRouter);
+
 const db = connectDb;
 db();
+
 app.listen(port, () =>
   console.log(`Server running on port: http://localhost:${port}`)
 );
 
+// Handle 404 errors
 app.all("*", (req, res) => {
-  res.status(404).json({ message: "end point does not exist" });
+  res.status(404).json({ message: "End point does not exist" });
 });
